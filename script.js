da7e3da8abc8f39ae6fd475b7bbb5d01c04c84d8
John Miller <jmiller55890@gmail.com>
2022-01-14 08:47:50 -0500

    added a prompt to get # of squares user would like for the total grid, executes on click of reset button

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 const gridContainer= document.querySelector(`.gridContainer`);
+squareCount='';
 
 
 // Function to create 16 square grid
-for (i=0; i<=16*16; i++){
+function gridCraft(value){
+    squareCount=value;
+for (i=0; i<=squareCount; i++){
     let gridSquare= document.createElement("div");
     gridSquare.classList.add("gridSquare");
     gridContainer.appendChild(gridSquare);
-}
-
+}};
 // Colors squares that have been moused over black
-const gridSpaces = document.querySelectorAll(`.gridSquare`);
-gridSpaces.forEach(space => {
-    space.addEventListener(`mouseover`, function() {
+let gridSpaces =``;
+
+function gridListener(){
+    gridSpaces = document.querySelectorAll(`.gridSquare`);
+    gridSpaces.forEach(space => {
+     space.addEventListener(`mouseover`, function() {
         space.classList.add("gridSquareBlack");
     });
-});
+})};
+
+gridCraft(256);
+gridListener();
+
+
+
 
 //The Reset Button
 const resetButton= document.querySelector(".resetButton");
 resetButton.addEventListener('click', function(){
     gridSpaces.forEach( cell =>{
-    cell.classList.remove("gridSquareBlack");    
-    })});
+    cell.remove();   
+    });
+
+    gridCraft(prompt("please enter the number of grid squares you would like.",256));
+    gridListener();
+});
 
 
 
